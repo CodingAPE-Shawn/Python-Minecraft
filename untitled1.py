de625ae573fd7e1c91fac78de18b7931d1d42527
Shawn <58027264+CodingAPE-Shawn@users.noreply.github.com>
2020-07-29 16:44:35 +0800

    day3

---
+++ untitled1.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul 29 14:48:42 2020
+
+@author: appedu
+"""
+
+from mcpi.minecraft import Minecraft
+mc = Minecraft.create()
+x,y,z = mc.player.getTilePos()
+def plantTree (x,y,z):
+    mc.setBlocks(x-1,y+3,z-1,x+1,y+5,z+1,leaves)
+    mc.setBlocks(x,y,z,x,y+4,z,trunk)
+    
+for i in range(10): 
